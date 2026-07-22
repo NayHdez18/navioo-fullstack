fbf2d56ae22056b47e2da76bfb15f26daea9e3b7
NayHdez18 <nobetton98@gmail.com>
2026-07-21 22:21:12 -0500

    cambios

--- backend/middleware/errorhandler.js
+++ backend/middleware/errorhandler.js
@@ -1,12 +1,35 @@
+const { json } = require ('express')
+const constants = require('../constants')
+
+
 const errorHandler = (err, req, res, next) => {
   console.error('Error detectado:', err);
 
-  const statusCode = err.statusCode || res.statusCode !== 200 ? res.statusCode : 500;
+  const statusCode =res.statusCode ? res.statusCode: 500;
+  console.log(statusCode);
+
+  let errorTitle ="";
+  switch(statusCode){
+    case constants.constants.VALIDATION_ERROR:
+            errorTitle = "Validation Field";
+            break;
+        case constants.constants.NOT_FOUND:
+            errorTitle = "Not found";
+            break;
+        case constants.constants.FORBIDDEN:
+            errorTitle = "Forbidden elements"
+            break;
+        case constants.constants.SERVER_ERROR:
+            errorTitle = "Server Error"
+            break;
+        default:
+            errorTitle = "Unknowkn Error"
+  }
 
   res.status(statusCode).json({
-    success: false,
-    message: err.message || 'Error interno del servidor',
-    stack: process.env.NODE_ENV === 'production' ? null : err.stack
+    title: errorTitle,
+    message: err.message,
+    stack: err.stack
   });
 };
 
