bbaec68c7d7781a03446ed6bcc6d14e5be087912
fernancho <2414006@utrivieramaya.edu.mx>
2026-07-23 19:59:53 -0500

    backend correction

--- backend/index.js
+++ backend/index.js
@@ -9,16 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const express = require('express');
-const app = express();
-const errorHandler = require('./middlewares/errorHandler');
+const errorHandler = require('./middleware/errorhandler');
 
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/entidad1', require('./routes/entidad1Routes'));
+app.use('/api', require('./routes/naviooroutes'));
 
 app.use(errorHandler);
-
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
